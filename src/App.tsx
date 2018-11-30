8daccaa9a8edb62bcf5ae80299496096c5a77918
rcdaniel <danielramirezconcepcion@gmail.com>
2018-11-30 19:25:55 +0000

    Starting App definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './containers/Header';
+import Home from './containers/Home';
+import About from './containers/About';
+import Movies from './containers/Movies';
 
-const App= () => {
-
-}
-
-export default App
+export default class App extends React.Component {}
